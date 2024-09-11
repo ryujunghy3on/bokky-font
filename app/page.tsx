6af18c65102ada9a3756d10bc446981e7f7369b6
ryujunghy3on <ryu@grida.co>
2024-09-11 21:40:43 +0900

    layout

--- app/page.tsx
+++ app/page.tsx
@@ -7,20 +7,24 @@ const custom = localFont({
   src: "./fonts/main.otf",
   display: "swap",
 });
-
 export default function Home() {
-  const [txt, setTxt] = useState<string>();
+  const [txt, setTxt] = useState<string>("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // 입력된 값을 대문자로 변환하여 저장
+    setTxt(e.target.value.toUpperCase());
+  };
+
   return (
     <main className="flex flex-col gap-40 items-center justify-center w-dvw h-dvh">
       <div className={custom.className}>
-        <p className="text-8xl font-bold">{txt}</p>
+        <p className="text-10xl">{txt}</p>
       </div>
       <input
         value={txt}
-        onChange={(e) => {
-          setTxt(e.target.value);
-        }}
-        className=""
+        onChange={handleChange}
+        className="border border-black p-2"
+        placeholder="Type En here"
       />
       <p>QUIRK</p>
     </main>
